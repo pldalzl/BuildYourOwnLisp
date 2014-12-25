84989c0a0c8c0e0ed1a83c9bb8e49bbf131845cc
pldalzl <pldalzl@gmail.com>
2014-12-25 12:57:59 +0000

    created prompt.c and added to repo

---
+++ prompt.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv){
+	/* Print Version and Exit information */
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl-c to Exit\n");
+
+	/* loop forever */
+	while(1){
+		
+		/* Output prompt and get input*/
+		char* input = readline("lispy> ");
+
+		/*Add input to history */
+		add_history(input);
+
+		/* Echo input */
+		printf("You said %s\n", input);
+
+		/* Free retrieved input */
+		free(input);
+	}
+	return 0;
+}
