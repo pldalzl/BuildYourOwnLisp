96c7cd45c3b72f55ef95aa28e2143ce305dfdfef
pldalzl <pldalzl@gmail.com>
2015-01-02 10:55:33 +0000

    renamed prompt.c to parsing.c - added  mpc.c, mpc.h to repo. added  parsing functions and a PN grammar

--- parsing.c
+++ parsing.c
@@ -10,6 +10,22 @@
 #include <string.h>
 
 static char buffer[2048];
+/* Create some parsers */ 
+mpc_parser_t* Number 	= mpc_new("number");
+mpc_parser_t* Operator 	= mpc_new("operator");
+mpc_parser_t* Expr 		= mpc_new("expr");
+mpc_parser_t* Lispy 	= mpc_new("lispy");
+
+/* Define them with the following Language */
+mpca_lang(MPCA_LANG_DEFAULT,
+	"														\
+	number 		: /-?[0-9]+/;								\
+	operator 	: '+' | '-' | '*' | '/';					\
+	expr 		: <number> | '(' <operator> <expr>+ ')' ;	\
+	lispy 		: /^/ <operator> <expr>+ /$/				\
+	",
+	Number, Operator, Expr, Lispy);
+
 
 /* Fake readline function */
 char* readline(char* prompt) {
@@ -34,28 +50,12 @@ void add_history(char* unused) {}
 mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
 int main(int argc, char** argv){
-	/* Create some parsers */ 
-	mpc_parser_t* Number 	= mpc_new("number");
-	mpc_parser_t* Operator 	= mpc_new("operator");
-	mpc_parser_t* Expr 		= mpc_new("expr");
-	mpc_parser_t* Lispy 	= mpc_new("lispy");
-
-	/* Define them with the following Language */
-	mpca_lang(MPCA_LANG_DEFAULT,
-		"														\
-		number 		: /-?[0-9]+/;								\
-		operator 	: '+' | '-' | '*' | '/';					\
-		expr 		: <number> | '(' <operator> <expr>+ ')' ;	\
-		lispy 		: /^/ <operator> <expr>+ /$/				\
-		",
-		Number, Operator, Expr, Lispy);
-
 	/* Print Version and Exit information */
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl-c to Exit\n");
 
 	/* loop forever */
-	while(1){		
+	while(1){
 		
 		/* Output prompt and get input*/
 		char* input = readline("lispy> ");
@@ -63,17 +63,8 @@ int main(int argc, char** argv){
 		/*Add input to history */
 		add_history(input);
 
-		/* Atempt to Parse user input */
-		mpc_result_t r;
-		if( mpc_parse("<stdin", input, Lispy, &r)){
-			/* on sucess print the AST */
-			mpc_ast_print(r.output);
-			mpc_ast_delete(r.output);			
-		}
-		else {
-			mpc_err_print(r.error);
-			mpc_err_delete(r.error);
-		}
+		/* Echo input */
+		printf("You said %s\n", input);
 
 		/* Free retrieved input */
 		free(input);
